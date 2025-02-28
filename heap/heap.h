f5237a47784a7f407b535d340876db171b945af2
wangchen <wangchen14@wangchen14deMacBook-Pro.local>
2025-02-28 17:38:24 +0800

    fix bug

--- heap/heap.h
+++ heap/heap.h
@@ -14,7 +14,7 @@ void up_heap(T *data, int pos, int n, CMP is_down){
 template<class T, class CMP>
 void down_heap(T *data, int pos, int n, CMP is_down){
     for(int c = (pos<<1) + 1; c < n; c = (pos<<1) + 1){
-        if(c + 1 < n && data[c+1] > data[c]){
+        if(c + 1 < n && is_down(data[c], data[c+1])){
             c = c + 1;
         }
         if(!is_down(data[pos], data[c])){
